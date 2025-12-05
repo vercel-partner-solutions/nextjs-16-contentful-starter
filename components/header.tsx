890c6f9287366926195875cb80e56c3b6abb0973
malewis5 <matthew.a.lewis7@gmail.com>
2025-12-05 15:45:18 -0500

    add link

--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 export const Header = ({ title }: { title: string }) => {
   return (
     <header className="border-b border-black/5 shadow-sm">
       <div className="max-w-4xl mx-auto px-6 py-8">
         <span className="text-2xl font-semibold tracking-tight text-black">
-          {title}
+          <Link href="/">{title}</Link>
         </span>
       </div>
     </header>
