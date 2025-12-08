34e9622c31560e6c62afc361e81d335906636437
Matthew Lewis <matthew.a.lewis7@gmail.com>
2025-12-08 15:31:44 -0500

    feat: use contentful sdk (#6)
    * use contentful sdk
    
    * better api signature
    
    * remove duplicate queries.
    
    * add comment

---
+++ lib/contentful/queries.ts
@@ -0,0 +1,22 @@
+import { getContentfulClient } from "./client";
+import { ArticleQuery, ArticleSkeleton, CONTENT_TYPE_IDS } from "./types";
+import { extractArticleFields } from "./utils";
+import { cacheTag } from "next/cache";
+
+export const getArticles = async (isDraft?: boolean, query?: ArticleQuery) => {
+  "use cache";
+  const client = getContentfulClient(isDraft);
+  const entriesResult =
+    await client.withoutUnresolvableLinks.getEntries<ArticleSkeleton>({
+      content_type: CONTENT_TYPE_IDS.KNOWLEDGE_ARTICLE,
+      ...query,
+    });
+  const entries = extractArticleFields(entriesResult);
+
+  // Cache using the sys.id. This ensures that if ANY article entry in the response is updated, the cache is invalidated.
+  cacheTag(
+    "articles",
+    entriesResult?.items?.map((entry) => entry.sys?.id).join(",")
+  );
+  return entries;
+};
